d8b24f3ce3f897c26846a1913dbbff3c000ba2ce
Tijani Anuoluwapo Sesede <anuoluwapotijani3@gmail.com>
2022-09-12 20:21:32 -0700

    codding

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,23 @@
 #include <stdio.h>
-
 /**
-* main - print the string
-*8**/
+
+* main - Printing
+
+*
+
+* Return: Always 0 (Successful)
+
+*/
+
 int main(void)
 
 {
 
-puts("\"Programming is like building a multilingual
+puts("\"Programming is like building a multilingual 
 
 puzzle");
-return (0)
+
+return (0);
+
 }
+
